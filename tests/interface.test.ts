148a6221ca5ae1efd123dc69fd881f1f245d349a
yat98 <hidayatchandra08@gmail.com>
2024-03-13 01:06:52 +0800

    add: function interface

--- tests/interface.test.ts
+++ tests/interface.test.ts
@@ -17,4 +17,15 @@ describe('Interface', () => {
     expect(seller).toHaveProperty('nib');
     expect(seller).toHaveProperty('npwp');
   });
+
+  it('should support function interface', () => {
+    interface AddFunction {
+      (value1: number, value2: number): number
+    }
+
+    const add: AddFunction = (value1: number, value2: number): number => value1 + value2;
+
+    expect(add(2,2)).toBe(4);
+    expect(add(2,3)).toBe(5);
+  });
 });
