63018a6806d21afa85102a50f3dfa10a0717d188
yat98 <hidayatchandra08@gmail.com>
2024-03-13 02:29:05 +0800

    add: extending interface

--- tests/interface.test.ts
+++ tests/interface.test.ts
@@ -1,3 +1,4 @@
+import { Employee, Manager } from "../src/employee";
 import { Seller } from "../src/interface";
 
 describe('Interface', () => {
@@ -61,5 +62,29 @@ describe('Interface', () => {
 
     expect(Object.keys(dictionary)[1]).toBe('address');
     expect(Object.values(dictionary)[1]).toBe('Indonesia');
+  });
+
+  it('should support extending interface', () => {
+    const employee: Employee = {
+      id: '1',
+      name: 'Hidayat',
+      division: 'IT'
+    };
+
+    const manager: Manager = {
+      id: '2',
+      name: 'Chandra',
+      division: 'IT',
+      numberOfEmployee: 10,
+    }
+
+    expect(employee).toHaveProperty('id');
+    expect(employee).toHaveProperty('name');
+    expect(employee).toHaveProperty('division');
+
+    expect(manager).toHaveProperty('id');
+    expect(manager).toHaveProperty('name');
+    expect(manager).toHaveProperty('division');
+    expect(manager).toHaveProperty('numberOfEmployee');
   });
 });
