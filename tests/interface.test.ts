36d2f27954e909dd3588694b4230941e55d015e0
yat98 <hidayatchandra08@gmail.com>
2024-03-13 02:14:26 +0800

    add: indexable interface

--- tests/interface.test.ts
+++ tests/interface.test.ts
@@ -28,4 +28,38 @@ describe('Interface', () => {
     expect(add(2,2)).toBe(4);
     expect(add(2,3)).toBe(5);
   });
+
+  it('should support indexable interface', () => {
+    interface StringArray {
+      [index: number]: string
+    }
+    
+    const names: StringArray = ['Hidayat', 'Chandra'];
+
+    Object.keys(names).map(Number)
+      .forEach(index => {
+        expect(typeof index).toBe('number')
+      });
+
+    Object.values(names).forEach(index => {
+      expect(typeof index).toBe('string')
+    });
+  });
+
+  it('should support indexable interface for non number index', () => {
+    interface StringDictionary {
+      [key: string]: string
+    } 
+
+    const dictionary: StringDictionary = {
+      'name': 'Hidayat',
+      'address': 'Indonesia',
+    }
+
+    expect(Object.keys(dictionary)[0]).toBe('name');
+    expect(Object.values(dictionary)[0]).toBe('Hidayat');
+
+    expect(Object.keys(dictionary)[1]).toBe('address');
+    expect(Object.values(dictionary)[1]).toBe('Indonesia');
+  });
 });
