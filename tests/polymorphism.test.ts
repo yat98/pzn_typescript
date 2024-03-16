a128a01bd4772efd7e5f69334bedd8e942fff61e
yat98 <hidayatchandra08@gmail.com>
2024-03-17 05:35:55 +0800

    add: polymorph

--- tests/polymorphism.test.ts
+++ tests/polymorphism.test.ts
@@ -12,16 +12,24 @@ describe('Polymorhpism', () => {
     expect(employee).toBeInstanceOf(Employee);
 
     function sayHello(employee: Employee): string {
-      return `Hello, ${employee.name}`;
+      if(employee instanceof VicePresident){
+        const vp = employee as VicePresident;
+        return `Hello vice president, ${vp.name}`;
+      }else if(employee instanceof Manager){
+        const manager = employee as Manager;
+        return `Hello manager, ${manager.name}`;
+      }else{
+        return `Hello employee, ${employee.name}`;
+      }
     }
 
     employee = new Employee('Yat');
-    expect(sayHello(employee)).toBe('Hello, Yat');
+    expect(sayHello(employee)).toBe('Hello employee, Yat');
 
     employee = new Manager('Eko');
-    expect(sayHello(employee)).toBe('Hello, Eko');
+    expect(sayHello(employee)).toBe('Hello manager, Eko');
 
     employee = new VicePresident('Ari');
-    expect(sayHello(employee)).toBe('Hello, Ari');
+    expect(sayHello(employee)).toBe('Hello vice president, Ari');
   });
 });
